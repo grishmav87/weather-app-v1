e4c7fbcf55ac999128bfaae6d904112b641f7df1
grishmav87 <112604862+grishmav87@users.noreply.github.com>
2022-09-18 01:04:12 +0800

    Implemented celsius to fahrenheit conversion

--- src/app.js
+++ src/app.js
@@ -29,9 +29,11 @@ function showCityInfo(event) {
   cityName.innerHTML = `${cityInput.value}`;
 
   function showCurrentWeather(response) {
-    let tempRound = Math.round(response.data.main.temp);
+    tempCelsius = response.data.main.temp;
+    let tempRound = Math.round(tempCelsius);
     let temperatureElement = document.querySelector("#tempvalue");
     temperatureElement.innerHTML = `${tempRound}`;
+
     let humidityRound = Math.round(response.data.main.humidity);
     let humidityElement = document.querySelector("#humidity");
     humidityElement.innerHTML = `Humidity: ${humidityRound}%`;
@@ -55,5 +57,31 @@ function showCityInfo(event) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showCurrentWeather);
 }
+
+function showTempFahrenheit(event) {
+  event.preventDefault();
+  tempCelsiusLink.classList.remove("active");
+  tempFahrenheitLink.classList.add("active");
+  let tempFahrenheitvalue = Math.round(tempCelsius * 1.8 + 32);
+  let temperatureElement = document.querySelector("#tempvalue");
+  temperatureElement.innerHTML = tempFahrenheitvalue;
+}
+
+function showTempCelsius(event) {
+  event.preventDefault();
+  tempFahrenheitLink.classList.remove("active");
+  tempCelsiusLink.classList.add("active");
+  let temperatureElement = document.querySelector("#tempvalue");
+  temperatureElement.innerHTML = Math.round(tempCelsius);
+}
+
 let searchCity = document.querySelector(".btn-primary");
 searchCity.addEventListener("click", showCityInfo);
+
+let tempFahrenheitLink = document.querySelector("#fahrenheit-main");
+tempFahrenheitLink.addEventListener("click", showTempFahrenheit);
+
+let tempCelsiusLink = document.querySelector("#celsius-main");
+tempCelsiusLink.addEventListener("click", showTempCelsius);
+
+let tempCelsius = null;
